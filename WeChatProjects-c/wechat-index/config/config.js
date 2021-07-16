2c8ae8facd1e27e91c9c3640011b228cdc032046
罗超 <9116367+lcxhcyn@user.noreply.gitee.com>
2021-07-16 15:40:09 +0000

    1

---
+++ WeChatProjects-c/wechat-index/config/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "my_menu": {
+      "pay": [{
+          "url":'',
+          "title": "支付",
+          "icon": "https://img.alicdn.com/tfscom/TB1JYKwNXXXXXbHXFXX1rlbFXXX.JPG_100x100.jpg"
+      }],
+      "collect": [{
+          "url":'',
+          "title": "收藏",
+          "icon": "https://img.alicdn.com/tfscom/TB1JYKwNXXXXXbHXFXX1rlbFXXX.JPG_100x100.jpg"
+          },
+          {
+              "url":'',
+              "title": "朋友圈",
+              "icon": "https://img.alicdn.com/tfscom/TB1JYKwNXXXXXbHXFXX1rlbFXXX.JPG_100x100.jpg"
+          },
+          {
+              "url":'',
+              "title": "卡包",
+              "icon": "https://img.alicdn.com/tfscom/TB1JYKwNXXXXXbHXFXX1rlbFXXX.JPG_100x100.jpg"
+          },
+          {
+              "url":'',
+              "title": "表情",
+              "icon": "https://img.alicdn.com/tfscom/TB1JYKwNXXXXXbHXFXX1rlbFXXX.JPG_100x100.jpg"
+          }
+      ],
+      "set":[{
+          "url":'',
+          "title": "设置",
+          "icon": "https://img.alicdn.com/tfscom/TB1JYKwNXXXXXbHXFXX1rlbFXXX.JPG_100x100.jpg"
+      }]
+  }
+}
