aafbed6bfe06109ac2f35fac41e80df6f771c4ab
罗超 <9116367+lcxhcyn@user.noreply.gitee.com>
2021-07-16 15:39:50 +0000

    1

---
+++ WeChatProjects-c/wechat-index/assets/db/db.js
@@ -0,0 +1,44 @@
+module.exports = {
+  "List": [
+    {
+      "id": "1",
+      "pic": "https://images5.alphacoders.com/587/thumb-1920-587597.jpg",
+      "title": "火山班",
+      "desc": "来 让我们拥抱未来",
+      "date": "刚刚"
+      
+    },
+    {
+      "id": "2",
+      "pic": "https://images5.alphacoders.com/587/thumb-1920-587597.jpg",
+      "title": "火山班",
+      "desc": "来 让我们拥抱未来",
+      "date": "刚刚"
+      
+    },
+    {
+      "id": "3",
+      "pic": "https://images5.alphacoders.com/587/thumb-1920-587597.jpg",
+      "title": "火山班",
+      "desc": "来 让我们拥抱未来",
+      "date": "刚刚"
+      
+    },
+    {
+      "id": "4",
+      "pic": "https://images5.alphacoders.com/587/thumb-1920-587597.jpg",
+      "title": "火山班",
+      "desc": "来 让我们拥抱未来",
+      "date": "刚刚"
+      
+    },
+    {
+      "id": "5",
+      "pic": "https://images5.alphacoders.com/587/thumb-1920-587597.jpg",
+      "title": "火山班",
+      "desc": "来 让我们拥抱未来",
+      "date": "刚刚"
+      
+    },
+  ]
+}
