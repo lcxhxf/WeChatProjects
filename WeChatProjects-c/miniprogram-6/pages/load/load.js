cfce754c988b8a6a668a21f9049eb82713fdbbcb
罗超 <9116367+lcxhcyn@user.noreply.gitee.com>
2021-07-23 04:41:12 +0000

    1

---
+++ WeChatProjects-c/miniprogram-6/pages/load/load.js
@@ -0,0 +1,99 @@
+const banners = [
+  {
+    id:1,
+    picUrl:'https://img1.baidu.com/it/u=2987668685,2324583552&fm=26&fmt=auto&gp=0.jpg',
+  },
+  {
+    id:2,
+    picUrl:'https://img2.baidu.com/it/u=2620400837,829953279&fm=26&fmt=auto&gp=0.jpg',
+  },
+  {
+    id:3,
+    picUrl:'https://img2.baidu.com/it/u=182724769,884243623&fm=26&fmt=auto&gp=0.jpg',
+  }
+]
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banners: [],
+    swiperCurrent:0,
+    swiperMaxNumber:0
+  },
+
+gotoIndex() {
+  wx.switchTab({
+    url: '/pages/index/index',
+  })
+},
+
+  swipechange(e) {
+    console.log(e.detail.current);
+    this.setData({
+      swiperCurrent:e.detail.current
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      banners,
+      swiperMaxNumber: banners.length
+
+
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
