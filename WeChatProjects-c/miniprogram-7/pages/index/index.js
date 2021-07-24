2c9c8f9003306c15043d4ca56155333b5ff8613e
罗超 <9116367+lcxhcyn@user.noreply.gitee.com>
2021-07-24 15:21:22 +0000

    1

---
+++ WeChatProjects-c/miniprogram-7/pages/index/index.js
@@ -0,0 +1,54 @@
+const banners = [
+  {
+    id:1,
+    picUrl:'http://img3m2.ddimg.cn/69/16/1862099682-1_l_3.jpg',
+  },
+  {
+    id:2,
+    picUrl:'http://img3m8.ddimg.cn/82/7/28977778-1_l_8.jpg',
+  },
+  {
+    id:3,
+    picUrl:'http://img3m3.ddimg.cn/15/12/28544883-1_l_78.jpg',
+  }
+]
+
+Page({
+  data: {
+    banners,
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
