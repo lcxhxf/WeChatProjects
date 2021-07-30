29568beb53bd72eeabe3113c21c7b4c415622a11
罗超 <2422635277@qq.com>
2021-07-30 15:44:08 +0800

    1

---
+++ WeChatProjects-c/yun_xy/miniprogram/components/avatar/index.js
@@ -0,0 +1,47 @@
+// components/avatar/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
+// components/avatar/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    src: String,
+    text: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- WeChatProjects-c/yun_xy/miniprogram/pages/me/index/index.js
+++ WeChatProjects-c/yun_xy/miniprogram/pages/me/index/index.js
@@ -1,32 +1,106 @@
-// miniprogram/pages/me/index/index.js
+// pages/me/index/index.js
+const db = wx.cloud.database();
+const _ = db.command;
+const userCollection = db.collection('users');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    isLogged:false
+    isLogged: false,
+    userName:'',
+    userImg:''
   },
   getUserInfo(e) {
+    console.log(userName, '-------');
     console.log(e);
-    const { nickName:userName, avatarUrl:userImg } =e.detail.userInfo;
+    const { nickName:userName, avatarUrl:userImg } = e.detail.userInfo;
+    // console.log(e.detail.signature, '+++++++++++');
+    let openId = e.detail.signature;
+    console.log(userName, '+++++++++++++');
     this.setData({
-      userName,
-      userImg,
-      isLogged:true
+      // userName,
+      // userImg,
+      isLogged: true
     })
     wx.setStorageSync('userName', userName);
+    console.log(userName, '!!!!!!!!!!!!!!!!!');
     wx.setStorageSync('userImg', userImg);
-    wx.setStorageSync('openId', e.detail.signature);
+    wx.cloud.callFunction({
+      name:'getOpenIdTest',
+      success:(res) => {
+        console.log(res);
+      }
+    })
+    wx.setStorageSync('openId', openId);
+    console.log(userName)
+    // add user 
+    userCollection
+      .where({
+        _openid: openId
+      })
+      .get() // 异步写法 回调
+      .then(res => {
+        console.log(res);
+        if (!res.data.length) {
+          let userId = this.getUserId();
+          userCollection
+            .add({
+              data: {
+                userName,
+                userImg,
+                userId
+              }
+            })
+            .then(res => {
+              // console.log()
+              wx.showToast({
+                title: '用户新增成功'
+              })
+            })
+        } else {
+          console.log('已存在');
+        }
+      }) // 同步写法 高级
   },
-  // getPhoneNumber(e) {
-
-  // }
+  getUserId() {
+    // users表的id 用户有一定的意义  vip 000
+    let firstW = "yf";
+    let userId = `${firstW}${Date.now() + (Math.random() * 1e5).toFixed(0)}`;
+    wx.setStorageSync('userId', userId);
+    return userId;
+  },
+  gotoMyTopic(e) {
+    console.log(e);
+    const userId = e.currentTarget.dataset.userid;
+    console.log(userId)
+    wx.navigateTo({
+      url: '../myTopic?userId=' + userId
+    })
+  },
+  gotoMyComment(e) {
+    const userId = e.currentTarget.dataset.userid;
+    console.log(userId)
+    wx.navigateTo({
+      url: '../myComment?userId=' + userId
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let openId = wx.getStorageSync('openId'),
+      userName = wx.getStorageSync('userName'),
+      userImg = wx.getStorageSync('userImg'),
+      userId = wx.getStorageSync('userId');
+    if (!openId) return 
+    this.setData({
+      isLogged: true,
+      userName,
+      userImg,
+      userId
+    })
   },
 
   /**
