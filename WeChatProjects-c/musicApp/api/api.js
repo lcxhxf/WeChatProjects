d1ddd6e26766acca1022991af920946ac858f1c7
罗超 <2422635277@qq.com>
2021-08-12 15:27:05 +0800

    api

---
+++ WeChatProjects-c/musicApp/api/api.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'http://localhost:3000';
+const request = (url, data) => { //地址 data 数据
+  let _url = BASE_URL + url;
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: '_url',
+      method: 'get',
+      data: data,
+      // 请求头 + 请求体 data
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success(res) {
+        resolve(res.data)
+      }
+    })
+  })
+}
+
+module.exports = {
+  getBanners() {
+    return request('/banner');
+  }
+}
