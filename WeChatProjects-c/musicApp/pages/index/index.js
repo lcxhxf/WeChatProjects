e79289e3222531bc91e616c9e02230fd5420e06b
罗超 <2422635277@qq.com>
2021-08-14 17:02:12 +0800

    1

--- WeChatProjects-c/musicApp/pages/index/index.js
+++ WeChatProjects-c/musicApp/pages/index/index.js
@@ -1,28 +1,103 @@
 // index.js
 // 获取应用实例
+// common.js
+// const { getBanners } = require('../../api/api');
+import API from '../../api/api'
+// console.log(API.getBanners)
 const app = getApp()
-
+let half = 0,
+  quarter = 0;
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    slideOffset: 0,
+    banners:[],
+    newsong: [],
+    newsong_index: [],
+    activeIndex: 0,
+    recommend_MV: []
   },
-  // 事件处理函数
-  bindViewTap() {
+  changeline(e) {
+    let current = e.detail.current;
+    // console.log(current);
+    if (current == 0) {
+      this.setData({
+        slideOffset: quarter - 14
+      })
+    } else {
+      this.setData({
+        slideOffset: quarter - 14 + half
+      })
+    }
+
+  },
+  go_search() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '/pages/search/search',
     })
   },
   onLoad() {
-    if (wx.getUserProfile) {
+    this.resetSlider();
+    this.loadBanners();
+    this.getNewSong();
+    this.songsheet();
+    this.getRecommendMV();
+    // this.getDjRadios();
+    // this.getNewEst();
+    // this.getProgramRecommend();
+  },
+  async getRecommendMV() {
+    const res = await API.getRecommendMV();
+    if (res.code == 200) {
       this.setData({
-        canIUseGetUserProfile: true
+        recommend_MV: res.result.slice(0, 4)
       })
     }
+  },
+  async songsheet() {
+    let res = await API.getsongsheet({
+      order: 'hot'
+    });
+    if (res.code == 200) {
+      this.setData({
+        songsheet: res.playlists,
+        songsheet_index: res.playlists.slice(0, 6)
+      })
+    }
+  },
+  async getNewSong() {
+    let res = await API.getNewSong();
+    // console.log(song);
+    if (res.code == 200) { // 请求成功
+      this.setData({
+        newsong: res.result,
+        newsong_index: res.result.slice(0, 6)
+      })
+    }
+  },
+  resetSlider() {
+    wx.getSystemInfo({
+      success:(res) => {
+        half = res.windowWidth / 2;
+        // console.log(half);
+        quarter = half / 2;
+        // console.log(half, quarter);
+        this.setData({
+          slideOffset: quarter - 14
+        })
+      }
+    })
+  },
+  async loadBanners() {
+    let res = await API.getBanners();
+    console.log(res, '------');
+    if (res.code == 200) {
+      this.setData({
+        banners: res.banners
+      })
+    }
+    // this.setData({
+    //   banners:
+    // })
   },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
